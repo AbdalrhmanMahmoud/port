c6c5aecc23f77e07712ad9cffb06e99ad526f7f3
Abdalrhman <abdalrhman18695@yahoo.com>
2020-11-18 05:42:33 +0200

    updat all

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp= require('gulp'),
+let gulp= require('gulp'),
     concat = require('gulp-concat'),
     prefix = require('gulp-autoprefixer'), /// add autoprefixer
     sass = require('gulp-sass'), /// sass tool 
     livereload = require('gulp-livereload'), 
-    sourcemaps = require('gulp-sourcemaps') /// sourcemaps make sourse map to make changise easy 
-    uglify = require('gulp-uglify'), // for commprise js 
+    sourcemaps = require('gulp-sourcemaps'), /// sourcemaps make sourse map to make changise easy 
+    // uglify = require('gulp-uglify-es') // for commprise js 
+    uglify = require('gulp-uglify-es').default,
+
     // notify = require("gulp-notify"), // motfcation form server 
     // zip = require('gulp-zip'), //// commpress fiels 
     // ftp = require( 'vinyl-ftp' ), // uplad to the host
-    // htmlmin = require('gulp-htmlmin'), //
+    htmlmin = require('gulp-htmlmin'); //
     // imagemin = require('gulp-imagemin'); //imagemin
      
 // css tasks
 gulp.task('css', function(){
-    return gulp.src('scss/main.scss')
+    return gulp.src('stage/scss/main.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(prefix('last 2 versions'))
@@ -25,42 +27,47 @@ gulp.task('css', function(){
 });
 //  js
 gulp.task('js', function(){
-    return gulp.src('projects/js/*.js')
-        .pipe(concat('main.js'))
+    return gulp.src('stage/js/mine.js')
+        // .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-        .pipe(notify(" js task finish "))
+        .pipe(gulp.dest('dist/javscript'))
+        // .pipe(notify(" js task finish "))
         .pipe(livereload())
-        .pipe(notify("jacascript update  don \n have fun !!  "))
+        // .pipe(notify("jacascript update  don \n have fun !!  "))
 });
 
+// gulp.task("uglify", function () {
+//         .pipe(rename("bundle.min.js"))
+//         .pipe(uglify(/* options */))
+//         .pipe(gulp.dest("lib/"));
+// });
 // compress images 
-// gulp.task('imagemin', () =>
-//     gulp.src('projects/images/*.*')
-//     .pipe(imagemin([
-//         imagemin.gifsicle({interlaced: true}),
-//         imagemin.jpegtran({progressive: true}),
-//         imagemin.optipng({optimizationLevel: 8}),
-//         imagemin.svgo({
-//             plugins: [
-//                 {removeViewBox: true},
-//                 {cleanupIDs: false}]
-//                     }) ]))
-//     .pipe(gulp.dest('dist/images'))
-//     .pipe(notify("images compress "))
+gulp.task('imagemin', () =>
+    gulp.src('/stage/images/*.*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 8}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}]
+                    }) ]))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify("images compress "))
 
-// );
+);
 
 // html fiels 
-// gulp.task("htmlmin", function(){
-//     return gulp.src('projects/index.html')
-//     .pipe(htmlmin({ collapseWhitespace: true }))
-//     .pipe(gulp.dest('dist'))
-//     .pipe(notify("html update ... "))
-//     .pipe(livereload())
+gulp.task("htmlmin", function(){
+    return gulp.src('stage/index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+    // .pipe(notify("html update ... "))
+    .pipe(livereload())
 
 
-// });
+});
 
 // compress fiels 
 // gulp.task("compress", function(){
@@ -92,8 +99,8 @@ gulp.task('js', function(){
 gulp.task('watch',function(){
     require("./server.js");
     livereload.listen();
-    gulp.watch('scss/*.scss',  gulp.series("css"));
-    gulp.watch('projects/js/*.js',  gulp.series("js"));
+    gulp.watch('stage/scss/*.scss',  gulp.series("css"));
+    gulp.watch('stage/js/*.js',  gulp.series("js"));
     // gulp.watch('projects/images/*.*',  gulp.series("imagemin"));
-    // gulp.watch('projects/*.html',  gulp.series("htmlmin"));
+    gulp.watch('stage/index.html',  gulp.series("htmlmin"));
 })
---
+++ stage/js/mine.js
@@ -0,0 +1,142 @@
+new WOW().init();
+
+let mainColors = localStorage.getItem("color_option")
+console.log(mainColors)
+
+if (mainColors !== null) {
+// console.log(mainColors)
+document.documentElement.style.setProperty('--main-color', mainColors)
+
+    document.querySelectorAll(".color-o li").forEach(element =>{
+
+        element.classList.remove("active");
+        if (element.dataset.color === mainColors){
+            element.classList.add('active')
+            console.log(element)
+        }
+    })
+
+
+}
+// setting-box
+// change fa-gear to span untel we gt conection 
+
+let gear = document.querySelector(" .toggle-setting .fa-gear");
+    gear.onclick = function (){
+
+    // this.classList.toggle('fa-spin');
+
+    document.querySelector(".setting-box").classList.toggle('open');
+}
+// switch color 
+const colorsList = document.querySelectorAll(".color-o li");
+console.log(colorsList)
+
+colorsList.forEach(li => {
+    li.addEventListener("click", (e) =>{
+        console.log( e.target.dataset.color)
+
+        document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
+        localStorage.setItem("color_option",  e.target.dataset.color)
+
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active")
+
+            e.target.classList.add("active")
+        })
+    })
+})
+
+
+// // select the main page
+// let page =document.querySelector(".mainxxx");
+
+// // images arry
+// let imageArray = ["01.jpg","02.jpg","03.jpg"];
+
+// // set the time 
+// setInterval(() => {
+//     // genirate the number 
+//     let randomN = Math.floor(Math.random() * imageArray.length);
+    
+//     page.style.backgroundImage = 'url("imgs/'+imageArray[randomN]+'")';
+    
+// }, 100000)
+// // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// let myImg =  ['01.jpg', '02.jpg']
+
+// let bgOPtion = true
+// function randomImg(){
+//     if (bgOPtion = true) {
+        
+//         setInterval(() => {
+//             const bgRandom = document.querySelectorAll(".randowm-bg span");
+
+// bgRandom.forEach(span => {
+//     span.addEventListener("click", (e) =>{
+
+
+
+//         e.target.parentElement.querySelectorAll(".active").forEach(element => {
+//             element.classList.remove("active")
+//         })
+
+//             e.target.classList.add("active")
+//     })
+// })
+
+//         }, 100);
+//     }
+// }zd
+
+// // switch bg 
+// randomImg()
+// 00000000000000000000000000000
+
+// gallery 
+let gallery = document.querySelectorAll(".gallery img")
+    gallery.forEach(img => {
+    img.addEventListener('click', (e) => {
+
+        let overlay = document.createElement("div");
+        overlay.className = "overlay-class";
+        document.body.appendChild(overlay)
+
+        let popupBox = document.createElement("div");
+        popupBox.className = "popupBox";
+
+        let popupImg = document.createElement("img")
+        popupImg.src = img.src;
+
+        popupBox.appendChild(popupImg)
+
+        document.body.appendChild(popupBox)
+
+        if (img.alt !== null) {
+            let imgHead = document.createElement("h4")
+            let imgText = document.createTextNode(img.alt)
+            imgHead.appendChild(imgText)
+            popupBox.appendChild(imgText)
+        }
+
+        let closepop = document.createElement("span")
+        let closepopText = document.createTextNode("X")
+        closepop.appendChild(closepopText)
+        closepop.className = ("closepop")
+        popupBox.appendChild(closepop)
+
+    });
+
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target.className == "closepop") {
+        e.target.parentNode.remove()
+    document.querySelector(".overlay-class").remove()
+        
+    }
+
+
+
+})
